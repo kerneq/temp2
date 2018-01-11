6880c0910283d005bf9385edc261a9aff86ab8d7
kerneq <stasik-0@yandex.ru>
2018-01-11 14:46:20 +0300

    make readData realization with Guava lib in DataReader class

--- src/main/java/ru/mail/polis/util/DataReader.java
+++ src/main/java/ru/mail/polis/util/DataReader.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.util;
 
+import com.google.common.io.ByteStreams;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.ByteArrayOutputStream;
@@ -11,16 +12,8 @@ import java.io.InputStream;
  */
 public class DataReader {
 
-    private static final int BUFFER_SIZE = 1024;
-
     public static byte[] readData(@NotNull InputStream is) throws IOException {
-        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
-            byte[] buffer = new byte[BUFFER_SIZE];
-            for (int len; (len = is.read(buffer, 0, BUFFER_SIZE)) != -1; ) {
-                os.write(buffer, 0, len);
-            }
-            os.flush();
-            return os.toByteArray();
-        }
+        return ByteStreams.toByteArray(is);
     }
+
 }
